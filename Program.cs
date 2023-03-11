de009e1301db6a3236387c3a275f880008c37b6f
Jefferson Abac <125109849+jefferson1fal@users.noreply.github.com>
2023-03-11 11:06:51 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.Write("Ingrese una palabra: ");
+string palabra = Console.ReadLine();
+
+if (EsPalindromo(palabra))
+{
+    Console.WriteLine($"{palabra} es un palíndromo");
+}
+else
+{
+    Console.WriteLine($"{palabra} no es un palíndromo");
+}
+    
+bool EsPalindromo(string palabra)
+{
+    palabra = palabra.ToLower(); 
+
+    int longitud = palabra.Length;
+    for (int i = 0; i < longitud / 2; i++)
+    {
+        if (palabra[i] != palabra[longitud - i - 1])
+        {
+            return false; 
+        }
+    }
+    return true; 
+}
